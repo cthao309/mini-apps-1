ffbaea3568aa7ab3cb06b9d0bbacc8f722a3c966
Charlie Thao <cthao309@gmail.com>
2020-04-20 11:22:44 -0700

    created logic to manipulate the game table

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,10 @@
 // object to contain the logic for the game
 let ticTacToeLogic = {
+  // initialize the game
+  initialize: function() {
+    this.renderTable();
+  },
+
   // table setting
   table:
   [
@@ -8,8 +13,11 @@ let ticTacToeLogic = {
     "", "", "",
   ],
 
-  // initialize table
-  initialize: function() {
+  // object to keep track of which square has been used
+  trackMove: {},
+
+  // render table
+  renderTable: function() {
     // grab the table id from the dom
     let tableId = document.getElementById('game_table');
 
@@ -18,7 +26,14 @@ let ticTacToeLogic = {
 
     // loop through the table array to generate the table
     for(let i = 0; i < table.length; i++) {
+      // create the element node
       let node = document.createElement('div');
+
+      // create the text node
+      let textNode = document.createTextNode(table[i]);
+
+      // append the text node to the element node
+      node.append(textNode);
 
       // add a class to the node
       node.classList.add('square');
@@ -26,20 +41,31 @@ let ticTacToeLogic = {
       // append the squares onto the dom
       tableId.append(node);
     }
+
   },
 
   // computer turn - logic
   aiMove: function() {
-    let squares = document.getElementsByClassName('square');
-    let randomMove = Math.floor(Math.random() * squares.length);
-    console.log('square => ', squares)
-    console.log('random => ', randomMove)
+    let randomMove = Math.floor(Math.random() * this.table.length);
+    console.log('computer turn => ', randomMove)
+
+    if(!this.trackMove[randomMove] && Object.keys(this.trackMove).length <= this.table.length) {
+      this.trackMove[randomMove] = true
+    } else {
+      this.aiMove();
+    }
+
+    console.log('tracker => ', this.trackMove)
+    this.appendToDom(randomMove, 'X');
   },
 
   // logic to append move to the dom
   appendToDom: function(index, turn) {
+    this.table[index] = turn;
 
-  }
+    console.log('table => ', this.table)
+    this.renderTable();
+  },
 }
 
 // initialize the tic-tac-toe game
