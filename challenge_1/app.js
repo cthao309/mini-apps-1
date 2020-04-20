cda6fc9d2ee5e7877e25ab9721555bb776013649
Charlie Thao <cthao309@gmail.com>
2020-04-20 16:32:56 -0700

    got score keeping in sync and generating a new board game when clicked on reset

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,17 +1,40 @@
-// object to contain the logic for the game
+// ######################################################################
+// Object to contain the logic for the game
+// ######################################################################
 let ticTacToeLogic = {
   // initialize the game
   initialize: function() {
+    document.getElementById('start').removeEventListener('click', ticTacToeLogic.initialize);
+
+    // render the table
     ticTacToeLogic.renderTable();
+
+    // render the score
+    ticTacToeLogic.renderScore();
   },
 
   // table setting
   table:
   [
-    "", "", "",
-    "", "", "",
-    "", "", "",
+    '', '', '',
+    '', '', '',
+    '', '', '',
   ],
+
+  // keeping track of wins
+  score: {
+    'X': 0,
+    'O': 0
+  },
+
+  // method to render the socre
+  renderScore: function() {
+    let xScore = document.getElementById('xWin');
+    xScore.innerText = ticTacToeLogic.score['X'];
+
+    let oScore = document.getElementById('oWin');
+    oScore.innerText = ticTacToeLogic.score['O'];
+  },
 
   // keep track of turn -- default (turn = false, first player turn 'X')
   turn: false,
@@ -23,8 +46,6 @@ let ticTacToeLogic = {
 
     ticTacToeLogic.turn = turn;
 
-    console.log('from toggleTurn ', turn)
-
     if(turn) {
       return 'X';
     } else {
@@ -35,14 +56,9 @@ let ticTacToeLogic = {
   // render table
   renderTable: function() {
     // grab the table id from the dom
-    let tableId = document.getElementById('game_table');
-
-    // clear the game table
-    let squares = document.getElementsByClassName('square');
+    let tableId = document.getElementById('board');
 
     let table = this.table;
-
-    console.log('table => ', table)
 
     // loop through the table array to generate the table
     for(let i = 0; i < table.length; i++) {
@@ -62,7 +78,6 @@ let ticTacToeLogic = {
       squareContainer.setAttribute('id', i);
 
       squareContainer.addEventListener('click', ticTacToeLogic.playerMove);
-          // cb()
       // append the squares onto the dom
       tableId.append(squareContainer);
     }
@@ -72,11 +87,6 @@ let ticTacToeLogic = {
   // computer turn - logic
   playerMove: function(e) {
 
-    console.log('this (playerMove) => ', this, this)
-    console.log('this (this.playerMove) => ', this.playerMove)
-
-    console.log('move => ', e.target, e.target.id);
-
     // grab the element being clicked
     let square = e.target;
 
@@ -85,8 +95,6 @@ let ticTacToeLogic = {
 
     // toggle between player
     let currentPlayer = ticTacToeLogic.toggleTurn();
-
-    console.log('turn => ', currentPlayer)
 
     // remove the click event listener on the square once it has been clicked
     square.removeEventListener('click', ticTacToeLogic.playerMove);
@@ -102,25 +110,19 @@ let ticTacToeLogic = {
       ticTacToeLogic.table[clickedSquareIndex] = currentPlayer;
     }
 
-    console.log('table => ', ticTacToeLogic.table)
-
     // check for winning combination
     ticTacToeLogic.checkWinning(currentPlayer);
   },
 
   // method for checking winning combination
   checkWinning: function(player) {
-    console.log('checkWinning => ', player)
-
     // winning combination
     // [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
     let table = ticTacToeLogic.table;
 
-    console.log(player === 'X')
-
     let case1 = (player === table[0] && player === table[1] && player === table[2]);
-    let case2 = (player === table[2] && player === table[3] && player === table[4]);
+    let case2 = (player === table[3] && player === table[4] && player === table[5]);
     let case3 = (player === table[6] && player === table[7] && player === table[8]);
     let case4 = (player === table[0] && player === table[3] && player === table[6]);
     let case5 = (player === table[1] && player === table[4] && player === table[7]);
@@ -129,34 +131,71 @@ let ticTacToeLogic = {
     let case8 = (player === table[2] && player === table[4] && player === table[6]);
 
     if(case1 || case2 || case3 || case4 || case5 || case6 || case7 || case8) {
-      // alert(`We have a winner: ${player} wins`)
+      ticTacToeLogic.score[player]++;
+
+      // grab the message element to display winning
       let message = document.getElementById('message');
 
+      // add text of winning
       message.innerText = `We have a winner. ${player} wins.`;
+
       let square = document.getElementsByClassName('square');
 
+      // loop to remove all the click event listener on the square once a winner is known
       for(let i = 0; i < square.length; i++) {
         square[i].removeEventListener('click', ticTacToeLogic.playerMove);
       }
+
+      // render new score sheet
+      ticTacToeLogic.renderScore();
     }
+  },
+
+  // method to clear the board game
+  clearBoard: function() {
+    // reset all the values in the table to empty string
+    for(let i = 0; i < ticTacToeLogic.table.length; i++) {
+      ticTacToeLogic.table[i] = '';
+    }
+
+    // grab the message element to display winning
+    let message = document.getElementById('message');
+
+    // add text of winning
+    message.innerText = ``;
+
+    // remove the old board
+    let board = document.getElementById('board');
+    board.remove();
+
+    let newBoard = document.createElement('div');
+    newBoard.setAttribute('id', 'board');
+
+    document.body.append(newBoard)
+
+    ticTacToeLogic.renderTable();
   }
 }
 
-
-// grab the start btn
-let startBtn = document.getElementById('start');
-
+// ######################################################################
+// Start the start logic
 // add a click event listener on the start btn to initiate the game
-startBtn.addEventListener('click', function(e) {
-  console.log('hello tic tac toe. Game is staring...');
-
-  // initialize the tic-tac-toe game
-  ticTacToeLogic.initialize();
-});
+// ######################################################################
+let startBtn = document.getElementById('start');
+startBtn.addEventListener('click', ticTacToeLogic.initialize);
 
 
-// reset the game
+// ######################################################################
+// Reset the game logic
+// ######################################################################
 let resetBtn = document.getElementById('reset');
-resetBtn.addEventListener('click', function() {
+resetBtn.addEventListener('click', ticTacToeLogic.clearBoard)
+
+
+// ######################################################################
+// Clear game score logic
+// ######################################################################
+let clearScoreBtn = document.getElementById('clearScore');
+clearScoreBtn.addEventListener('click', function() {
   window.location.reload();
 })
