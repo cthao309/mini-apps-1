c045476edc4a2b58f990a8ba6d5b8fac3b526b64
Charlie Thao <cthao309@gmail.com>
2020-04-21 11:06:26 -0700

    created the UI and end-point for the form submission

---
+++ challenge_2/controllers/json2csv.js
@@ -0,0 +1,5 @@
+const convertJson2csv = function(req, res) {
+  console.log('console log from the json2csv file...', req.body)
+}
+
+module.export = convertJson2csv;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,13 +1,27 @@
+// loading server script and node module dependencies
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const json2csv = require('./controllers/json2csv');
 
 const app = express();
 const PORT = 3000;
 
+// enable middleware for processing data from client
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+// enable middleware for serving static file, index.html
 app.use(express.static(path.join(__dirname, '/client')));
 
+// end point for post request
+app.post('/json2csv', (req, res) => {
+  console.log('json2csv end-point')
+  res.send('json2csv end-point')
+})
 
-
+// event listener on the application at 3000
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`)
 })
