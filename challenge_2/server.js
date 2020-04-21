747943824e326f9fe49a13ce51635a956cf0a1e0
Charlie Thao <cthao309@gmail.com>
2020-04-21 10:40:01 -0700

    initial commit for challenge_2

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`)
+})
 
