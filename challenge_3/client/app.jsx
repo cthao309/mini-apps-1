b7391351c96aae31fdb0cf8613a35312152cad5a
Charlie Thao <cthao309@gmail.com>
2020-04-22 13:34:18 -0700

    finished setting up babel

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      form1: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className="multistep_checkout">
+        <button>Check out</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -7,6 +7,11 @@ const PORT = 3000;
 // add express middleware
 app.use(express.static(path.join(__dirname, '/public')));
 
+// root end-point
+app.get('/', (req, res) => {
+  res.send(path.join(__dirname, '/../public/index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is starting at http://localhost:${PORT}`);
