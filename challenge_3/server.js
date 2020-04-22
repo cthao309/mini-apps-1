c86a2d3d3e6cfa5a87da8d20981679196ef6813e
Charlie Thao <cthao309@gmail.com>
2020-04-22 10:24:41 -0700

    initial commit for multistep checkout: challenge 3

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// add express middleware
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.listen(PORT, () => {
+  console.log(`Server is starting at http://localhost:${PORT}`);
+});
 
 
