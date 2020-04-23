4ce161b31ed05dbdd7352fdeb9f829a23b6bb07c
Charlie Thao <cthao309@gmail.com>
2020-04-23 09:54:27 -0700

    initial commit for connect four

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
+// enable express middleware to serve static file
+app.use(express.static(path.join(__dirname, './client/dist')))
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
 
